a26aaba2d6049ec217e5fcf42dde7bcadfc87b31
Sudharshan <72936645+sudharshan2001@users.noreply.github.com>
2022-09-02 21:00:55 +0530

    Add files via upload

---
+++ embeddings.py
@@ -0,0 +1,14 @@
+
+class TAPEmbedding(layers.Layer):
+    def __init__(self, maxlen, vocab_size, embed_dim):
+        super(TAPEmbedding, self).__init__()
+        self.token_emb = layers.Embedding(input_dim=vocab_size, output_dim=embed_dim)
+        self.pos_emb = layers.Embedding(input_dim=maxlen, output_dim=embed_dim)
+
+    def call(self, x):
+        maxlen = tf.shape(x)[-1]
+        positions = tf.range(start=0, limit=maxlen, delta=1)
+        positions = self.pos_emb(positions)
+        x = self.token_emb(x)
+        return x + positions
+
